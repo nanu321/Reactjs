c3a7f71d852f6fa753b895c329939de0259ca247
Prasad <gawde.p@resoftech.com>
2022-03-24 15:49:37 +0530

    Video 12

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+//import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState('dark'); //whether dark mode is enabled or not.
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" mode={mode} aboutText="About TextUtils" />
       <div className="container my-3">
-      {/* <TextForm heading="Enter the text to analyze below"/> */}
-      <About/>
+      <TextForm heading="Enter the text to analyze below"/>
+      {/* <About/> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,6 +23,18 @@ export default function TextForm(props) {
   const handleOnChange = (event)=>{
     setText(event.target.value)
   }
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
   const [text, setText] = useState('');
   return (
     <>
@@ -35,6 +47,8 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lowercase</button>
         <button className="btn btn-primary mx-2" onClick={handleReverseClick}>Reverse Text</button>
         <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
       </div>
       <div className="container my-3">
         <h1>Your text summary</h1>
