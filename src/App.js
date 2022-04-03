e3c377bec44b6bddf338de8c9ea48469cee56290
nanu321 <93853694+nanu321@users.noreply.github.com>
2022-04-03 13:01:36 +0530

    video 16,17 routing and build app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-//import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled or not.
@@ -23,21 +29,41 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
       showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
     }else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
     }
   }
 
   return (
     <>
+    <Router>
       <Navbar title="TextUtils" mode={mode} aboutText="About TextUtils" toggleMode={toggleMode} />
       <Alert alert={alert}/>
       <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-      {/* <About/> */}
+      <Switch>
+        {/* Why to use path
+          /users --> Components 1
+          /users/home --> Components 2
+        */}
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+        </Route>
+      </Switch>
       </div>
+    </Router>
     </>
   );
 }
